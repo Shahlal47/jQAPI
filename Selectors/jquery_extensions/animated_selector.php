38a951d4d5beda80a55b93ea46fccca569b91aa7
Shahlal47 <shahlalhossain@gmail.com>
2015-01-30 08:58:22 +0600

    Practices the basic jquery functions.

---
+++ Selectors/jquery_extensions/animated_selector.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>jQuery practice</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/resources/demos/style.css">
+
+    <style>
+        div {
+            background: yellow;
+            border: 1px solid #AAA;
+            width: 80px;
+            height: 80px;
+            margin: 0 5px;
+            float: left;
+        }
+        div.colored {
+            background: green;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<button id="run">Run</button>
+<div></div>
+<div id="mover"></div>
+<div></div>
+
+<script>
+    $( "#run" ).click(function() {
+        $( "div:animated" ).toggleClass( "colored" );
+    });
+    function animateIt() {
+        $( "#mover" ).slideToggle( "slow", animateIt );
+    }
+    animateIt();
+</script>
+
+</body>
+</html>
---
+++ Selectors/jquery_extensions/gt_selector.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>jQuery practice</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/resources/demos/style.css">
+
+    <style>
+
+    </style>
+
+</head>
+
+<body>
+
+<table border="1">
+    <tr><td>TD #0</td><td>TD #1</td><td>TD #2</td></tr>
+    <tr><td>TD #3</td><td>TD #4</td><td>TD #5</td></tr>
+    <tr><td>TD #6</td><td>TD #7</td><td>TD #8</td></tr>
+</table>
+
+<script>
+    $( "td:gt(4)" ).css( "backgroundColor", "yellow" );
+    $( "td:gt(-2)" ).css( "color", "red" );
+</script>
+
+</body>
+</html>
